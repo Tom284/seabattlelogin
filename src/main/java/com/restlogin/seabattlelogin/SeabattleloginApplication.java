95caf1e9f8091c4742acb37a3df5a6c70bb5252f
Tom284 <55879415+Tom284@users.noreply.github.com>
2020-10-26 10:55:18 +0100

    setup

---
+++ src/main/java/com/restlogin/seabattlelogin/SeabattleloginApplication.java
@@ -0,0 +1,13 @@
+package com.restlogin.seabattlelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeabattleloginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeabattleloginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restlogin/seabattlelogin/SeabattleloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restlogin.seabattlelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeabattleloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
